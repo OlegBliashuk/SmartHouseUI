cb5a2926b4976917a4a3bc841080f726ffa3479b
Oleg Bliashuk <blyashuk7@gmail.com>
2015-12-23 03:00:14 +0200

    0,1 version of app

---
+++ SmartHApp/SmartHApp/SmartHApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SmartHApp.MvcApplication" Language="C#" %>
---
+++ SmartHApp/SmartHApp/SmartHApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmartHApp.Startup))]
+namespace SmartHApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
